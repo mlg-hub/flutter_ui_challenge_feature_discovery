a1c136a87e59cf726daac99ece0ea5b34b1cc22f
Matt Carroll <me@mattcarroll.name>
2018-05-10 16:49:37 -0700

    Quickly build test bed UI.

--- lib/main.dart
+++ lib/main.dart
@@ -21,14 +21,124 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
+        leading: new IconButton(
+          icon: new Icon(
+            Icons.menu,
+          ),
+          onPressed: () {
+            // TODO:
+          }
+        ),
         title: new Text(''),
+        actions: <Widget>[
+          new IconButton(
+              icon: new Icon(
+                Icons.search,
+              ),
+              onPressed: () {
+                // TODO:
+              }
+          ),
+        ],
       ),
-      body: new Center(),
+      body: new Content(),
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(
+          Icons.add,
+        ),
+        onPressed: () {
+          // TODO:
+        },
+      ),
+    );
+  }
+}
+
+class Content extends StatefulWidget {
+  @override
+  _ContentState createState() => new _ContentState();
+}
+
+class _ContentState extends State<Content> {
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      children: <Widget>[
+        new Column(
+          children: <Widget>[
+            // Header photo
+            new Image.network(
+              'https://www.balboaisland.com/wp-content/uploads/Starbucks-Balboa-Island-01.jpg',
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: 200.0,
+            ),
+
+            // About the business
+            new Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(16.0),
+              color: Colors.blue,
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: new Text(
+                      'Starbucks Coffee',
+                      style: new TextStyle(
+                        color: Colors.white,
+                        fontSize: 24.0,
+                      ),
+                    ),
+                  ),
+                  new Text(
+                    'Coffee Shop',
+                    style: new TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // Button to start feature discovery
+            new Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(16.0),
+              child: new RaisedButton(
+                child: new Text('Do Feature Discovery'),
+                onPressed: () {
+                  // TODO:
+                },
+              ),
+            )
+          ],
+        ),
+
+        new Positioned(
+          top: 200.0,
+          right: 0.0,
+          child: new FractionalTranslation(
+            translation: const Offset(-0.5, -0.5),
+            child: new FloatingActionButton(
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.blue,
+              child: new Icon(
+                Icons.drive_eta,
+              ),
+              onPressed: () {
+                // TODO:
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
