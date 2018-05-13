f935a4c7c820e8e36693508ef6bf1d4362d92751
Matt Carroll <me@mattcarroll.name>
2018-05-12 22:55:22 -0700

    Create AnchoredOverlay that builds a UI in an overlay centered atop another Widget. Break out widgets into layout.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:overlays/layout.dart';
 
 void main() => runApp(new MyApp());
 
@@ -23,53 +24,37 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return new OverlayBuilder(
-      showOverlay: false,
-      overlayBuilder: (BuildContext context) {
-        return new CenterAbout(
-          position: new Offset(200.0, 500.0),
-          child: new Container(
-            width: 50.0,
-            height: 50.0,
-            decoration: new BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.purple,
-            ),
-          ),
-        );
-      },
-      child: new Scaffold(
-        appBar: new AppBar(
-          backgroundColor: Colors.green,
-          leading: new IconButton(
-            icon: new Icon(
-              Icons.menu,
-            ),
-            onPressed: () {
-              // TODO:
-            },
-          ),
-          title: new Text(''),
-          actions: <Widget>[
-            new IconButton(
-              icon: new Icon(
-                Icons.search,
-              ),
-              onPressed: () {
-                // TODO:
-              },
-            ),
-          ],
-        ),
-        body: new Content(),
-        floatingActionButton: new FloatingActionButton(
-          child: new Icon(
-            Icons.add,
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.green,
+        leading: new IconButton(
+          icon: new Icon(
+            Icons.menu,
           ),
           onPressed: () {
             // TODO:
           },
         ),
+        title: new Text(''),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(
+              Icons.search,
+            ),
+            onPressed: () {
+              // TODO:
+            },
+          ),
+        ],
+      ),
+      body: new Content(),
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(
+          Icons.add,
+        ),
+        onPressed: () {
+          // TODO:
+        },
       ),
     );
   }
@@ -147,108 +132,6 @@ class _ContentState extends State<Content> {
               ),
             )),
       ],
-    );
-  }
-}
-
-class OverlayBuilder extends StatefulWidget {
-  final bool showOverlay;
-  final Function(BuildContext) overlayBuilder;
-  final Widget child;
-
-  OverlayBuilder({
-    this.showOverlay = false,
-    this.overlayBuilder,
-    this.child,
-  });
-
-  @override
-  _OverlayBuilderState createState() => new _OverlayBuilderState();
-}
-
-class _OverlayBuilderState extends State<OverlayBuilder> {
-  OverlayEntry overlayEntry;
-
-  @override
-  void initState() {
-    super.initState();
-
-    if (widget.showOverlay) {
-      showOverlay();
-    }
-  }
-
-  @override
-  void didUpdateWidget(OverlayBuilder oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    syncWidgetAndOverlay();
-  }
-
-  @override
-  void reassemble() {
-    super.reassemble();
-    syncWidgetAndOverlay();
-  }
-
-  @override
-  void dispose() {
-    if (isShowingOverlay()) {
-      hideOverlay();
-    }
-
-    super.dispose();
-  }
-
-  bool isShowingOverlay() => overlayEntry != null;
-
-  void showOverlay() {
-    overlayEntry = new OverlayEntry(
-      builder: widget.overlayBuilder,
-    );
-    addToOverlay(overlayEntry);
-  }
-
-  void addToOverlay(OverlayEntry entry) async {
-    Overlay.of(context).insert(entry);
-  }
-
-  void hideOverlay() {
-    overlayEntry.remove();
-    overlayEntry = null;
-  }
-
-  void syncWidgetAndOverlay() {
-    if (isShowingOverlay() && !widget.showOverlay) {
-      hideOverlay();
-    } else if (!isShowingOverlay() && widget.showOverlay) {
-      showOverlay();
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return widget.child;
-  }
-}
-
-class CenterAbout extends StatelessWidget {
-  final Offset position;
-  final Widget child;
-
-  CenterAbout({
-    this.position,
-    this.child,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return new Positioned(
-      top: position.dy,
-      left: position.dx,
-      child: new FractionalTranslation(
-        translation: const Offset(-0.5, -0.5),
-        child: child,
-      ),
     );
   }
 }
